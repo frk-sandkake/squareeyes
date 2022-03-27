340a3f9e8c45e33c735d8647013d0582598d6ee9
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-03-27 03:54:58 +0200

    small changes on footer, header, forms. Hotjar removed from index to check if it interfere with modal

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const modal = document.getElementById("signUpModal");
-const btnSignUp = document.getElementById("btnModal");
+const btnSignUp = document.querySelector("#btnModal");
+const iconSignUp = document.querySelector(".btn-modal");
 const span = document.getElementsByClassName("close")[0];
 const btnSuccess = document.getElementById("successMessage")
 const alertMessage = document.querySelector(".alert__container");
 const signUpField = document.querySelector(".sign-up-fieldset");
 
 btnSignUp.onclick = function() {
+  modal.style.display = "block";
+}
+
+iconSignUp.onclick = function() {
   modal.style.display = "block";
 }
 
